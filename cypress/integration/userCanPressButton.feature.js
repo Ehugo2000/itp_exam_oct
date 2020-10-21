e06903d40b9fbf14c82307d70129ff32326f67f8
ehugo <mihaivos@gmail.com>
2020-10-21 14:41:18 +0200

    3rd test, tryin if we get a greeting message. Works if I put the javascipt src in <head> and with defer, not if javascript src is in body...

--- cypress/integration/userCanPressButton.feature.js
+++ cypress/integration/userCanPressButton.feature.js
@@ -3,4 +3,7 @@ describe ('user can press a button', () => {
         cy.visit('http://localhost:3001')
         cy.get('#quote').click()
     } )
+    it('display greeting when click button', () => {
+        cy.get('#message').should('contain', 'hello')
+    })
 })
